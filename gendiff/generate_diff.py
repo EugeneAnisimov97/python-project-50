9804ef904a0674964fb86a88ef88401cef392717
Eugene Anisimov <zero0061@mail.ru>
2024-04-10 20:16:31 +0200

    fix generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -3,26 +3,34 @@ import os
 import itertools
 
 
-def generate_diff(file1, file2):    #gendiff 'tests/files/file1.json' 'tests/files/file2.json'
+def to_str(value):
+    if isinstance(value, bool):
+        if value:
+            return f'true'
+        else:
+            return f'false'
+    return value
+
+
+def generate_diff(file1, file2):    #gendiff 'tests/fixtures/file1.json' 'tests/fixtures/file2.json'
     with (
         open(file1, 'r') as file1,
         open(file2, 'r') as file2,
     ):
         data1 = json.load(file1)
         data2 = json.load(file2)
-        items = []
-        # keys = data1.keys() | data2.keys()
-        for key, value in data1.items():
-            if key in data2:
-                if data1[key] == data2[key]:
-                    items.append(f'  {key}: {value}')
-                else:
-                    items.append(f'+ {key}: {value}')
-                    items.append(f'- {key}: {data2[key]}')
-            else:
-                items.append(f'- {key}: {value}')
-        for key, value in data2.items():
+        result = ['{']
+        keys = data1.keys() | data2.keys()
+        for key in sorted(keys):
             if key not in data1:
-                items.append(f'+ {key}: {value}')
-        result = itertools.chain('{', items, '}')
+                result.append(f'  + {key}: {to_str(data2[key])}')
+            elif key not in data2:
+                result.append(f'  - {key}: {to_str(data1[key])}')
+            elif data1[key] != data2[key]:
+                result.append(f'  - {key}: {to_str(data1[key])}')
+                result.append(f'  + {key}: {to_str(data2[key])}')
+            else:
+                result.append(f'    {key}: {to_str(data2[key])}')
+        result[-1] = '}'
         return '\n'.join(result)
+    
