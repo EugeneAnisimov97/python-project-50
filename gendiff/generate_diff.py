4e574b6a3689a6f6f85399992cd1f8840981ea51
Eugene Anisimov <zero0061@mail.ru>
2024-04-12 07:54:15 +0200

    fix func in parsing and diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,28 @@
 from gendiff.parsing import to_txt
 
 
+def to_str(value):
+    if isinstance(value, bool):
+        if value:
+            return 'true'
+        else:
+            return 'false'
+    return value
+
+
 def generate_diff(file1, file2):    # gendiff 'tests/fixtures/file1.json' 'tests/fixtures/file2.json' gendiff 'tests/fixtures/filepath1.yml' 'tests/fixtures/filepath2.yml'  # noqa: E501
-    return to_txt(file1, file2)
+    data1, data2 = to_txt(file1, file2)
+    result = ['{']
+    keys = data1.keys() | data2.keys()
+    for key in sorted(keys):
+        if key not in data1:
+            result.append(f'  + {key}: {to_str(data2[key])}')
+        elif key not in data2:
+            result.append(f'  - {key}: {to_str(data1[key])}')
+        elif data1[key] != data2[key]:
+            result.append(f'  - {key}: {to_str(data1[key])}')
+            result.append(f'  + {key}: {to_str(data2[key])}')
+        else:
+            result.append(f'    {key}: {to_str(data2[key])}')
+    result.append('}')
+    return '\n'.join(result)
--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -3,30 +3,30 @@ import yaml
 import os
 
 
-def to_str(value):
-    if isinstance(value, bool):
-        if value:
-            return 'true'
-        else:
-            return 'false'
-    return value
+# def to_str(value):
+#     if isinstance(value, bool):
+#         if value:
+#             return 'true'
+#         else:
+#             return 'false'
+#     return value
 
 
-def get_content(data1, data2):
-    result = ['{']
-    keys = data1.keys() | data2.keys()
-    for key in sorted(keys):
-        if key not in data1:
-            result.append(f'  + {key}: {to_str(data2[key])}')
-        elif key not in data2:
-            result.append(f'  - {key}: {to_str(data1[key])}')
-        elif data1[key] != data2[key]:
-            result.append(f'  - {key}: {to_str(data1[key])}')
-            result.append(f'  + {key}: {to_str(data2[key])}')
-        else:
-            result.append(f'    {key}: {to_str(data2[key])}')
-    result.append('}')
-    return '\n'.join(result)
+# def get_content(data1, data2):
+#     result = ['{']
+#     keys = data1.keys() | data2.keys()
+#     for key in sorted(keys):
+#         if key not in data1:
+#             result.append(f'  + {key}: {to_str(data2[key])}')
+#         elif key not in data2:
+#             result.append(f'  - {key}: {to_str(data1[key])}')
+#         elif data1[key] != data2[key]:
+#             result.append(f'  - {key}: {to_str(data1[key])}')
+#             result.append(f'  + {key}: {to_str(data2[key])}')
+#         else:
+#             result.append(f'    {key}: {to_str(data2[key])}')
+#     result.append('}')
+#     return '\n'.join(result)
 
 
 def to_json(file1, file2):
@@ -36,7 +36,7 @@ def to_json(file1, file2):
     ):
         data1 = json.load(file1)
         data2 = json.load(file2)
-        return get_content(data1, data2)
+        return data1, data2
 
 
 def to_yaml(file1, file2):
@@ -46,7 +46,7 @@ def to_yaml(file1, file2):
     ):
         data1 = yaml.safe_load(file1)
         data2 = yaml.safe_load(file2)
-        return get_content(data1, data2)
+        return data1, data2
 
 
 def to_txt(file1, file2):
